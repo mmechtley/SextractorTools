c59e592e2383bc2193c8f6dd27324155474228ce
Matt Mechtley <mmechtley@gmail.com>
2016-01-27 00:15:38 -0500

    Fixed error in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='SextractorTools',
     version='1.0',
-    packages=[''],
+    packages=['SextractorTools'],
     url='',
     license='BSD',
     author='Matt Mechtley',
